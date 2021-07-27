1417af134da976f51a83c6f8abf6eb7c5f673d6d
inesessetti <inesessetti99@gmail.com>
2021-07-28 00:54:20 +0100

    vh avatar love you

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons';
-import logo from './logo.jpg';
+import logo from './logo.png';
+import avatar from './avatar.png';
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -17,31 +18,41 @@ function Navbar() {
       <IconContext.Provider value={{ color: '#fff' }}>
         <div className='navbar'>
           <Link to='#' className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar} />
+            <FaIcons.FaBars onClick={showSidebar} className='bars' style={{float:'left',boxSizing:'border-box'}}/>
           </Link>
+          <div className='site-title'><h1>Auto école SITI</h1></div>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
+          <ul className='nav-menu-items'>
             <li className='navbar-toggle'>
               <Link to='#' className='menu-bars'>
-                <AiIcons.AiOutlineClose />
+                <div style={{marginLeft:'150px'}}><FaIcons.FaBars className='bars' onClick={showSidebar} /></div>
               </Link>
             </li>
-            <div >
+            <Link to='/'>
+            <div>
               <img className="logo" src={logo} />
             </div>
+            <div className='Title'>
+              <h3>Auto ecole Siti</h3>
+            </div>
+            </Link>
+            <hr className='line'/>
+            <div style={{marginTop:'30px'}}>
             {SidebarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
-                  <Link to={item.path}>
+                  <Link to={item.path} onClick={showSidebar}>
                     {item.icon}
                     <span>{item.title}</span>
                   </Link>
                 </li>
               );
             })}
+            </div>
           </ul>
           <div className="side-menu-footer">
+          <img className="avatar" src={avatar} className="avatar"/>
             <div className="user-info">
               <h5>Ines Essetti</h5>
               <p>inesessetti99@gmail.com</p>
